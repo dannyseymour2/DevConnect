4ef3ecd266f28c263f561dc2f08386c197756f7b
dannyseymour <daniel@discordia.io>
2020-10-29 11:48:16 -0500

    added auth middleware

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,9 @@ const express = require('express');
 const router = express.Router();
 const {check, validationResult} = require('express-validator');
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 
 const User = require('../../models/User');
 
@@ -27,18 +30,50 @@ router.post('/',[
             email
         }); 
         if (user){
-            res.status(400).json({
-                errors: errors.array()
+            return res.status(400).json({
+                errors: [
+                    {
+                        msg: 'User Already Exists'
+                    }
+                ]
             });
         }
+        const avatar = gravatar.url(email,{
+            s: '200',
+            r: 'pg',
+            d: 'mm'
+        });
 
+        user = new User({
+            name,
+            email,
+            avatar,
+            password
+        });
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        await user.save();
 
+        const payload = {
+            user:{
+                id: user.id
+            }
 
-    //Encrypt password
+        };
+        jwt.sign(
+            payload, 
+            config.get("jwtSecret"),
+            {
+                expiresIn: 360000
+            },
+            (err, token)=>{
+                if(err){
+                    throw err;
+                } 
+                res.json({token});
+            }
+            );
 
-    //Return jsonwebtoken
-
-    res.send('User Route');
     }catch(err){
         console.error(err.message);
         res.status(500).send('Server Error');
